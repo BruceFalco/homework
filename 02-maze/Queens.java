73d6afa1a7c1124027a2c628160f9be13b0c9238
tjieluo.capri <tjieluo.capri@gmail.com>
2015-02-25 21:44:27 -0500

    done

--- 02-maze/Queens.java
+++ 02-maze/Queens.java
@@ -12,6 +12,11 @@ public class Queens {
     private char danger = '.';
     // danger = all squares in danger of the placed queenies
 
+    private int numq = 0;
+    // num of queens on board
+    private int numdot = 0;
+    // num of squares in danger
+
     public void delay(int n) {
 	try {
 	    Thread.sleep(n);
@@ -44,21 +49,62 @@ public class Queens {
     }
 
     public void solve () {
-	int numdot = 0;
-	// number of squares that are in danger
-	int numq = 0;
-	// number of queens on board
 
-	if (numdot + numq == length * length) {
+	boolean placing = true;
+	boolean morespace = true;
+
+
+	if (numdot + numq == length*length) {
+	    placing = false;
+	}
+
+	if (placing == false) {
 	    System.out.println(this);
 	    return;
 	}
-	
-	ArrayList<Integer> badrow = new ArrayList<Integer>();
 
-	//for (
+	// if all squares are filled, return
+	for (int i = 0; i < length && placing == true ; i ++ ) {
+	    for (int j = 0 ; j < length && placing == true ; j ++ ) {
+		if (board[i][j] == empty) {
+		    board[i][j] = queenie;
+		    placing = false;
+		    makebad(i,j);
+		    numq = numq + 1;
+		}
+	    }
+	}
+
+	solve();
 	
-    }	
+	//	if (placing == false) {
+	//  System.out.println(this);
+	//  return;
+    }
+   
+	
+	
+   	
+
+    public void makebad(int h, int k) {
+	for (int i = 0 ; i < length; i ++ ) {
+	    for (int j = 0 ; j < length; j ++) {
+		if ((i == h || k==j || h-k==i-j || h+k == i+j )
+		    && board[i][j] == empty ) {
+		    board[i][j] = danger;
+		    numdot = numdot + 1;
+		}
+	    }
+	}
+    }
+
+
+    public static void main(String[] args) {
+	Queens q = new Queens(5);
+	System.out.println(q);
+	q.solve();
+	System.out.println(q);
+    }
 
 
 }
