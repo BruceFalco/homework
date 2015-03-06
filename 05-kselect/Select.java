58563230fc66794dd8b785f2226d93946aee9fcf
tjieluo.capri <tjieluo.capri@gmail.com>
2015-03-05 23:37:44 -0500

    to be d

---
+++ 05-kselect/Select.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Select {
+
+    ArrayList<Integer> bin = new ArrayList<Integer>;
+
+    int SI = 0;
+
+    public partition() {
+	ArrayList<Integer> herr = new ArrayList<Integer>;
+	for (int i = 0; i < bin.size() ; i++) {
+	    
+
+	}
+
+	// Schlaf ist schon, ist es nicht?
+
+    }
+}
