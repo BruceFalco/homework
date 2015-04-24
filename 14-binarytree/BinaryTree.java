a8ce310ab7f8c3f08bf79039922e9d6425d3d7eb
tjieluo.capri <tjieluo.capri@gmail.com>
2015-04-23 21:26:02 -0400

    need print method

--- 14-binarytree/BinaryTree.java
+++ 14-binarytree/BinaryTree.java
@@ -10,8 +10,44 @@ public class BinaryTree {
     }
 
     public Node search(Node T, int i) {
-	
+	Node T2 = T;
 	while (T! = null) {
+	    T2 = T;
 	    int c = T.getData().compareTo(i);
 	    if (c > 0) {
-		
+		T = T.getRight();
+	    }
+	    else if (c < 0) {
+		T = T.getLeft();
+	    }
+	    else {
+		return T;
+	    }
+	}
+	return T2;
+    }
+
+    public void insert(Node t, int i) {
+	Node base;
+	// from which new leaf springs
+
+	if (root == null) {
+	    root = t;
+	}
+	base = search(t, i);
+
+	int c = base.getData().compareTo(i);
+	if (c > 0) {
+	    base.right.setData(i);
+	}
+	else if (c < 0) {
+	    base.left.setData(i);
+	}
+	
+	
+
+	// print it
+
+    }
+
+}
